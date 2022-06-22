a2a6646df6f0bacbf567cc18be294b180b72aaba
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-06-22 23:18:59 +0300

    Designing service card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-
+import About from "./components/About/About";
+import Service from "./components/Service/Service";
 const App = () => {
   return (
     <>
       <Navbar />
       <Home/>
+      <About/>
+      <Service/>
     </>
   );
 };
