1f7d313bf189c0a721859c448f467c3e78605d3e
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-06-25 23:47:26 +0300

    Adding Pagination

--- src/components/Career/Career.js
+++ src/components/Career/Career.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "./career.css";
 import DataInfo from "./DataInfo";
+import Pagination from "../Pagination/Pagination";
 
 const Career = () => {
   const url = "https://jsonplaceholder.typicode.com/posts";
 
   const [data, setData] = useState([]);
+
+
+  //for pagination
+
+  const PER_PAGE = 7;
+  const [currentPage,setCurrentPage] = useState(1);
+  const handlePageClick = ({selected: selectedPage})=>{
+    setCurrentPage(selectedPage)
+  }
+
+
+  const offset = currentPage * PER_PAGE;
+  const currentPageData = data.slice(offset,offset+PER_PAGE);
+
+  const pageCount = Math.ceil(data.length/PER_PAGE);
+
 
   const loadPostData = () => {
     fetch(url, {
@@ -27,7 +44,7 @@ const Career = () => {
   }, []);
 
   return (
-    <div className="container career">
+    <div className="container career" id="career">
       <div className="section_title">
         <h5>Career</h5>
         <span className="line"></span>
@@ -35,7 +52,7 @@ const Career = () => {
 
       <div className="row">
         {data &&
-          data.map((item, index) => (
+          currentPageData.map((item, index) => (
             <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
               <DataInfo key={index} {...item}/>
 
@@ -43,6 +60,11 @@ const Career = () => {
             </div>
           ))}
       </div>
+
+      <div className="pagination-details">
+        <Pagination pageCount={pageCount} handlePageClick={handlePageClick}/>
+      </div>
+
     </div>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -49,7 +49,19 @@ const Navbar = () => {
             Service
           </Link>
         </li>
-        <li className="nav-item">Career</li>
+
+        <li className="nav-item">
+          <Link
+            to="career"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={100}
+          >
+            Career
+          </Link>
+        </li>
+
         <li className="nav-item">
           <Link
             to="video"
